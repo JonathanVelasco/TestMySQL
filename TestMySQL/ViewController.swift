8f937b905034b40e97d5c48ebe672690b3ddd7a8
Jonathan Velasco <j0a6v6v6@yahoo.es>
2018-06-10 13:59:46 -0400

    Initial Commit

---
+++ TestMySQL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestMySQL
+//
+//  Created by Jonathan Velasco Vargas on 10/6/18.
+//  Copyright © 2018 Jonathan Velasco Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
